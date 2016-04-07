34cefddc5666acfa1fe783f01e616a8a347e7bc6
JeffBaucom <jeffbaucom@gmail.com>
2016-04-07 10:03:11 -0400

    implement local auth, no sessions

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,4 +3,8 @@ var mongojs = require('mongojs');
 module.exports = function(app, express, db) {
 	var router = express.Router();
 	
+	router.route('/')
+		.post(function (req, res, next) {
+
+		});
 };
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var assert = require('assert');
 var path = require('path');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
+var crypto = require('crypto');
+var morgan = require('morgan');
 
 var app = express();
 
@@ -20,13 +20,10 @@ app.use(function(req, res, next) {
 });
 
 app.use(morgan('dev'));
-app.use(require('cookie-parser')());
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-
 
 var dbUrl = 'mongodb://localhost:27017/mean-cats';
 var collections = ['cats', 'users', 'corrals'];
-var db = mongojs.connect(dbUrl, collections);
+var db = mongojs(dbUrl, collections);
 
 db.on('connect', function () {
     console.log('database connected');
@@ -34,11 +31,59 @@ db.on('connect', function () {
 
 app.use(express.static(__dirname + '/public'));
 
-var userRoutes = require('./api/routes/users')(app, express, db);
-app.use('/api/users', userRoutes);
+app.get('/login', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/views/login.html'));
+});
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/views/index.html'));
+app.post('/login', function(req, res) {
+  if (!req.body.user || !req.body.pass) {  
+    res.send('Username and password both required');
+    return;
+  }
+
+  crypto.randomBytes(128, function (err, salt) {
+    if (err) { throw err; }
+    db.users.findOne({user: req.body.user}, function(err, doc) {
+    	if (err) {res.send('there was an error logging in'); return;}
+    	if (!doc) {res.send('could not find user'); return;}
+	    crypto.pbkdf2(req.body.pass, doc.salt, 7000, 256, 
+	      function (err, hash) {
+	        if (err) { throw err; }
+	        	if (err) {res.send('there was an error logging in'); return;}
+
+	        	if (!doc) {res.send('could not find user'); return;}
+	        	if (doc.salt && (doc.hash === (new Buffer(hash).toString('hex')))) {
+
+	        		res.send('you are now logged in as ' + req.body.user);
+	        	}
+	           });
+	      });
+  });
+
+});
+
+app.get('/register', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/views/regform.html'));
+});
+
+app.post('/register', function (req, res) {
+  if (!req.body.user || !req.body.pass) {  
+    res.send('Username and password both required');
+    return;
+
+  }
+  
+  crypto.randomBytes(128, function (err, salt) {
+    if (err) { throw err; }
+    salt = new Buffer(salt).toString('hex');
+    crypto.pbkdf2(req.body.pass, salt, 7000, 256, 
+      function (err, hash) {
+        if (err) { throw err; }
+        db.users.save({user: req.body.user, 
+        	salt : salt, hash : (new Buffer(hash).toString('hex'))});
+        res.send('Thanks for registering ' + req.body.user);
+      });
+  });
 });
 
 app.listen(3000, function() {
